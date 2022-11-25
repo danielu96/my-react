d35a5dad2fe382397a030e1f023ec4228a654f7d
Daniel <danielu96@wp.pl>
2022-11-25 19:57:07 +0100

    fixed display CardModal

--- src/Pages/CardModal.js
+++ src/Pages/CardModal.js
@@ -22,7 +22,7 @@ import {
 
 
 
-const Modal = ({ id, amount, quantity }) => {
+const Modal = ({ data, id, amount, quantity }) => {
 
     const [basicModal, setBasicModal] = useState(false);
     const dispatch = useDispatch();
@@ -30,134 +30,53 @@ const Modal = ({ id, amount, quantity }) => {
     const toggleShow = () => setCentredModal(!centredModal);
     const [centredModal, setCentredModal] = useState(false);
     const [currentItems, setCurrentItems] = useState([]);
-
     const [availableProducts, setAvailableProducts] = useState();
     const [calculation, setCalculation] = useState(0);
     const [count, setCount] = useState(0);
     const [input, setInput] = useState("");
-    const [data, setData] = useState([]);
-    const apiGet = () => {
-        fetch("../DATA/data.json")
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data);
-            });
-    };
-    useEffect(() => {
-        apiGet();
-    }, []);
+    const navigate = useNavigate();
+    const handleOnClick = () => navigate(-1);
+
     return (
         <>
-            <aside className="modal-container-cart" >
-                <div className='modal-cart'>
-                    remove all
-                </div>
-                <button onClick={() => {
-                    dispatch(clearCart());
-                    dispatch(closeModal());
-                }}>confirm</button>
-                <button onClick={() => {
-
-                    dispatch(closeModal());
-                }}>cancel</button>
-            </aside>;
-
-
-
-            <div className='container'>
-                {/* <MDBBtn onClick={toggleShow}>LAUNCH DEMO MODAL</MDBBtn> */}
-                {/* <MDBModal show={basicModal} setShow={setBasicModal} tabIndex='-1'>
-                    <MDBModalDialog>
-                        <MDBModalContent>
-                            <MDBModalHeader>
-                                <MDBModalTitle>Modal title</MDBModalTitle>
-                                <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-                            </MDBModalHeader>
-                            <MDBModalBody>Modal body text goes here.</MDBModalBody>
-
-                            <MDBModalFooter>
-                                <MDBBtn color='secondary' onClick={toggleShow}>
-                                    Close
-                                </MDBBtn>
-                                <MDBBtn>Save changes</MDBBtn>
-                            </MDBModalFooter>
-                        </MDBModalContent>
-                    </MDBModalDialog>
-                </MDBModal> */}
-
-
-
-
-                {/* {
-
-                    data.
-                        filter((card) => card.title === title)
-                        .map((card) => (
-                            <div className='task' key={card.id} >
-                                <p>{card.title}</p>
-                                <span>{card.cena}</span>
-                                <p>dostępne: {availableProducts} szt.</p>
-                                <span>wartość zamówienia {calculation} zł </span>
-                                <label style={{ width: '500px' }}>Treść nadruku <input type="text" value={input} onChange={(e) => setInput(e.target.value)} /></label>
-                                <p className='amount'>{count}</p>
-                            </div>
-                        ))
-                } */}
-            </div>
-            <div>
+            <div className="d-container-cart">
                 {data
                     .filter((card) => card.title === title)
                     .map((card, id) => (
                         <div key={id}>
-                            <MDBModal tabIndex='-1' show={centredModal} setShow={setCentredModal}>
-                                <MDBModalDialog centered>
-                                    <MDBModalContent>
-                                        <MDBModalHeader>
-                                            <MDBModalTitle > <h1 style={{ paddingLeft: "2.1rem" }}>{card.title}</h1></MDBModalTitle>
-                                            <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-                                        </MDBModalHeader>
-                                        <MDBModalBody>
-                                            <div className="opis">{card.opis}</div>
-                                            <img className="Image" src={`${card.Image}`} alt="kubek czarny"></img>
 
-                                        </MDBModalBody>
-                                        <MDBModalFooter>
-                                            <button disabled={amount === quantity ? true : false} className="btn_cart" onClick={() => {
-                                                dispatch(increase({ id }));
-                                            }}>+</button>
-                                            <p>{amount}</p>
-                                            <button className="btn_cart" onClick={() => {
-                                                if (amount === 1) {
-                                                    dispatch(removeItem(id));
-                                                    return;
-                                                }
-                                                dispatch(decrease({ id }));
-                                            }}>-</button>
-                                            <Link className=" btn" onClick={toggleShow} to={`/NowaLista/${card.title}`}> Kup</Link>
+                            <MDBModalDialog centered>
+                                <MDBModalContent>
+                                    <MDBModalHeader>
+                                        <MDBModalTitle > <h1 style={{ paddingLeft: "2.1rem" }}>{card.title}</h1></MDBModalTitle>
+                                        <MDBBtn className='btn-close' color='none' onClick={handleOnClick}></MDBBtn>
+                                    </MDBModalHeader>
+                                    <MDBModalBody>
+                                        <div className="opis">{card.opis}</div>
+                                        <img className="Image" src={`${card.Image}`} alt="kubek czarny"></img>
 
-                                            <MDBBtn color='primary' onClick={toggleShow}>
-                                                Close
-                                            </MDBBtn>
-
-                                        </MDBModalFooter>
-                                    </MDBModalContent>
-                                </MDBModalDialog>
-                            </MDBModal>
+                                    </MDBModalBody>
+                                    <MDBModalFooter>
+                                        <button disabled={amount === quantity ? true : false} className="btn_cart" onClick={() => {
+                                            dispatch(increase({ id }));
+                                        }}>+</button>
+                                        <p>{card.amount}</p>
+                                        <button className="btn_cart" onClick={() => {
+                                            if (amount === 1) {
+                                                dispatch(removeItem(id));
+                                                return;
+                                            }
+                                            dispatch(decrease({ id }));
+                                        }}>-</button>
+                                        <Link className=" btn" onClick={toggleShow} to={`/NowaLista/${card.title}`}> Kup</Link>
+                                        <MDBBtn color='primary' onClick={handleOnClick}>
+                                            Close
+                                        </MDBBtn>
+                                    </MDBModalFooter>
+                                </MDBModalContent>
+                            </MDBModalDialog>
                         </div>))}
             </div>
-
-            {/* <div className='container'>
-
-                {
-                    data.
-                        filter((card) => card.title === title)
-                        .map((card, id) => (
-                            <div key={id}>
-                                {card.title}
-
-
-                            </div>))}
-            </div> */}
         </>
     );
 }
