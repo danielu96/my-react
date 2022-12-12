340e0a13353e4c2db29a6c58c1fc468cee4151c0
Daniel <danielu96@wp.pl>
2022-12-12 13:13:05 +0100

    cleaning

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,20 @@
-// import { applyMiddleware } from 'redux';
 import { configureStore, createReducer } from '@reduxjs/toolkit'
-
-
-// import { validateAuthorMiddleware } from '../middleware/validateAuthorMiddleware';
-
-
 import cartReducer from '../Cart/cartSlice';
+import cardReducer from '../Pages/CardSlice';
 import modalReducer from '../Cart/modalSlice';
+import CardModalReducer from '../Pages/CardModalSlice';
+import kartyReducer from "../KARTY/kartySlice";
+
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    card: cardReducer,
     modal: modalReducer,
+    CardModal: CardModalReducer,
+    karty: kartyReducer,
+
   },
-
-
 });
 
 export default store;
