3fe19b08d2672c4999eea85534c6dcb7e7e9e05d
Daniel <danielu96@wp.pl>
2022-12-12 13:56:14 +0100

    deleted toFixed

--- src/Cart/CartContainer.js
+++ src/Cart/CartContainer.js
@@ -1,12 +1,11 @@
 import CartItem from './CartItem';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { openModal } from './modalSlice';
-import { fontSize } from '@mui/system';
+
 
 const CartContainer = () => {
     const dispatch = useDispatch();
-    const { cartItems, total, amount } = useSelector((state) => state.cart);
+    const { cartItems, amount, total } = useSelector((state) => state.cart);
     if (amount < 1) {
         return (
             <h2>nie ma nic </h2>
@@ -23,7 +22,7 @@ const CartContainer = () => {
                     })}
                 </div>
                 <hr />
-                <span>TOTAL {total.toFixed(2)}</span>
+                <span>TOTAL {total}</span>
 
                 <button className='btn_my' onClick={() =>
                     dispatch(openModal())}>Clear Cart</button>
@@ -34,3 +33,5 @@ const CartContainer = () => {
 }
 
 export default CartContainer;
+
+{/* <span>TOTAL {totalCena.toFixed(2)}</span> */ }
