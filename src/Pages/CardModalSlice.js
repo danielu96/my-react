d02c029c49bb5e588450d28a5045198821e8056d
Daniel <danielu96@wp.pl>
2022-12-12 13:37:18 +0100

    added increase and decrease

--- src/Pages/CardModalSlice.js
+++ src/Pages/CardModalSlice.js
@@ -1,11 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import cartItems from '../Cart/cartItems';
+
+const url = "../DATA2/data2.json"
 
 const initialState = {
+    cartItems: cartItems,
     isOpen: false,
+    amount: 0,
+    total: 0,
 };
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+    return fetch(url)
+        .then((resp) => resp.json())
+        .catch((err) => console.log(err));
+});
 
 const CardModalSlice = createSlice({
-    name: 'modal',
+    name: 'CardModal',
     initialState,
     reducers: {
         openModal: (state, action) => {
@@ -14,7 +25,27 @@ const CardModalSlice = createSlice({
         closeModal: (state, action) => {
             state.isOpen = false;
         },
+        increase: (state, { payload }) => {
+            const CardModal = state.cartItems.find((item) => item.id
+                === payload.id)
+            CardModal.amount = CardModal.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id
+                === payload.id)
+            cartItem.amount = cartItem.amount - 1;
+        },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount
+                total += item.amount * item.cena
+            })
+            state.amount = amount
+            state.total = total
+        }
     },
 });
-export const { openModal, closeModal } = CardModalSlice.actions;
+export const { openModal, closeModal, increase, decrease, calculateTotals } = CardModalSlice.actions;
 export default CardModalSlice.reducer;
