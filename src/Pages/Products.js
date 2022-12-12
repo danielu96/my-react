a3cef98d3bc52e38b023262dc4c9012be7b0c46e
Daniel <danielu96@wp.pl>
2022-12-12 14:04:57 +0100

    cleaning

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,38 +1,13 @@
 import React from "react";
-
-import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from "./CardSlice";
-import { useEffect } from "react";
-
-// import cartItems from "../Cart/cartItems";
 import CartItem from "./FullCard";
+import cartItems from "../Cart/cartItems";
 const Products = () => {
-    const { cardItems, total, amount } = useSelector((store) => store.card);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(calculateTotals());
-
-    }, [cardItems]);
-
-    if (amount < 1) {
-        return (
-            <h2>nie ma nic </h2>
-        );
-    }
-
     return (
-        <div>
+        <div className="container">
 
-
-
-            {cardItems.map((item) => {
+            {cartItems.map((item) => {
                 return <CartItem key={item.id}{...item} />
-
-
-
             })}
-            <span>TOTAL {total}</span>
         </div>
     );
 }
