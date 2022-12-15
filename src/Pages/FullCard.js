74ef1dfab171f7a3513e0033b8eff9078fe7ed22
Daniel <danielu96@wp.pl>
2022-12-15 16:41:37 +0100

    added count

--- src/Pages/FullCard.js
+++ src/Pages/FullCard.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { removeItem, increase, decrease, AddCart, } from "../Cart/cartSlice";
+import { removeItem, AddCart, } from "../Cart/cartSlice";
+import { increase, decrease } from "./CardSlice";
 import { Link } from "react-router-dom";
+
 
 
 const CartItem = ({ id, title, cena, amount, quantity, availableProducts }) => {
 
   const navigate = useNavigate();
   const handleOnClick = () => navigate(-1);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(0);
   console.log(navigate);
 
   const handleAdd = (id) => {
@@ -19,7 +22,7 @@ const CartItem = ({ id, title, cena, amount, quantity, availableProducts }) => {
         title: title,
         cena: cena,
         quantity: quantity,
-        amount: amount,
+        amount: count,
         availableProducts: availableProducts,
       }
       ))
@@ -31,24 +34,36 @@ const CartItem = ({ id, title, cena, amount, quantity, availableProducts }) => {
         <div className="item_cena"> <p >{cena} zł</p></div>
         <div className="item_quantity"> <p>dostępne {availableProducts} szt</p></div>
         <div className="item3">
-          <button disabled={amount === availableProducts ? true : false} className="btn_cart" onClick={() => {
+          {/* <button disabled={amount === availableProducts ? true : false} className="btn_cart" onClick={() => {
             dispatch(increase({ id }));
-          }}>+</button>
+          }}>+</button> */}
+          <button disabled={count === availableProducts ? true : false}
+            className='btn_my' style={{ width: "50px" }} onClick={() => setCount((c) => c + 1)}>+</button>
+
         </div>
-        <div className="item_amount"> <p>{amount}</p></div>
+        <div className="item_amount">
+          {/* <p>{amount}</p> */}
+          <p>{count} </p>
+        </div>
         <div className="item5">
-          <button className="btn_cart" onClick={() => {
+          {/* <button className="btn_cart" onClick={() => {
             if (amount === 1) {
               dispatch(removeItem(id));
               return;
             }
             dispatch(decrease({ id }));
-          }}>-</button>
+          }}>-</button> */}
+          <button disabled={count === 0 ? true : false} className='btn_my'
+            style={{ width: "50px" }} onClick={() => setCount((c) => c - 1)}>-</button>
+
         </div>
 
         <div className="item6">
+
+
         </div>
         <div className="item7">
+
         </div>
         <Link className='btn_remove' onClick={() => handleAdd(id)} to={`/Cart`}>
           dodaj
