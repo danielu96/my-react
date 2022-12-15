b3280f6c339e3349760909495d7fafa1d103ed82
Daniel <danielu96@wp.pl>
2022-12-15 16:44:08 +0100

    disable cardReducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,16 +2,16 @@ import { configureStore, createReducer } from '@reduxjs/toolkit'
 import cartReducer from '../Cart/cartSlice';
 import cardReducer from '../Pages/CardSlice';
 import modalReducer from '../Cart/modalSlice';
-import CardModalReducer from '../Pages/CardModalSlice';
+// import CardModalReducer from '../Pages/CardModalSlice';
 import kartyReducer from "../KARTY/kartySlice";
 
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    card: cardReducer,
+    // card: cardReducer,
     modal: modalReducer,
-    CardModal: CardModalReducer,
+    // CardModal: CardModalReducer,
     karty: kartyReducer,
 
   },
