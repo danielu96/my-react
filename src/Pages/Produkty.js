c0ee22d07af88d747bc84e57e955a90e0dda79ea
Daniel <danielu96@wp.pl>
2022-12-19 13:18:06 +0100

    created Produkty

---
+++ src/Pages/Produkty.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import products from './cardItems';
+
+const Produkty = () => {
+    return (
+        <div>
+            <h1>PRODUCTS</h1>
+            {products.map((product) => {
+                return (
+                    <article key={product.id}>
+                        <h5>{product.title}</h5>
+                        <p>{product.cena}</p>
+                        <p>{product.availableProducts} dostępne</p>
+                        {/* <Link to={`/Produkty/${product.id}`}>more info</Link> */}
+                        <Link to={`/Produkty/${product.title}`}>more info</Link>
+                    </article>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Produkty;
