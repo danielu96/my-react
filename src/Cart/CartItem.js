162aa2c93f25ec882b405c67c0593f5111e5d6b3
Daniel <danielu96@wp.pl>
2023-01-10 12:26:29 +0100

    FaTrashAlt added

--- src/Cart/CartItem.js
+++ src/Cart/CartItem.js
@@ -1,35 +1,50 @@
 import { removeItem, increase, decrease } from "./cartSlice";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { FaPlus, FaMinus, FaTrashAlt } from "react-icons/fa";
 const CartItem = ({ id, title, amount, cena, availableProducts, max }) => {
     const dispatch = useDispatch()
-
-
     return (
         <div className="cart-item">
             <div className="item_title">  <h4>{title}</h4></div>
             <div className="item_cena"> <p >{cena} zł szt.</p></div>
             <div className="item_quantity"> <span>zamówione {amount} szt.</span></div>
             <div className="item3">
-                <button className="btn_cart" onClick={() => {
+                {/* <button className="btn_cart" onClick={() => {
                     dispatch(increase({ id }));
-                }}>+</button>
+                }}>+</button> */}
             </div>
-            <div className="item_amount"> <p>{amount}</p></div>
-            <div className="item5">
-                <button className="btn_cart" onClick={() => {
+            <div className="item_amount">
+                <button className="amount-btn" onClick={() => {
+                    dispatch(increase({ id }));
+                }} ><FaPlus /></button>
+                {amount}
+                <button className='amount-btn' onClick={() => {
                     if (amount === 1) {
                         dispatch(removeItem(id));
                         return;
                     }
                     dispatch(decrease({ id }));
-                }}>-</button>
+                }}><FaMinus /></button>
+
+
+
+            </div>
+            <div className="item5">
+                {/* <button className="btn_cart" onClick={() => {
+                    if (amount === 1) {
+                        dispatch(removeItem(id));
+                        return;
+                    }
+                    dispatch(decrease({ id }));
+                }}>-</button> */}
             </div>
             <div className="item6">
                 <button className='btn_remove' onClick={() => {
                     dispatch(removeItem(id));
                 }}
-                > remove </button>
+                >  <  FaTrashAlt /></button>
+
             </div>
             <div className="item7">
 
