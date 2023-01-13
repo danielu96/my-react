b8d58320d82ad9cf22503ffbfa82d48e7f7f0fd1
Daniel <danielu96@wp.pl>
2023-01-13 17:25:32 +0100

    Auth0Slice disabled

---
+++ src/Cart/Auth0Slice.js
@@ -0,0 +1,22 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// const initialState = {
+//     isLoading: false,
+// }
+
+// const Auth0Slice = createSlice({
+//     name: 'Auth0',
+//     initialState,
+//     reducers: {
+
+//         logout: (state, action) => {
+//             state.isLoading = false;
+//         },
+//     },
+// });
+
+
+
+// export const { logout } = Auth0Slice.actions;
+// export default Auth0Slice.reducer;
