346985cb06315b884261413c935269d982c622d7
Daniel <danielu96@wp.pl>
2023-01-17 10:43:53 +0100

    change Redirect on Link

--- src/Pages/PrivateRoute.js
+++ src/Pages/PrivateRoute.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react"
 const PrivateRoute = ({ children, ...rest }) => {
     const { user } = useAuth0()
-    return (<Route
-        {...rest}
-        render={() => {
-            return user ? children : <Redirect to='/'></Redirect>
-        }} ></Route>);
+    return (
+        <Route
+            {...rest}
+            render={() => {
+                return user ? children : <Link to='/'></Link>
+            }} ></Route>);
 }
 
 export default PrivateRoute;
