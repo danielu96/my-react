d4009800e7b38b6187f5a2544a4f69d02b287b24
Daniel <danielu96@wp.pl>
2023-01-17 10:42:39 +0100

    AuthWrapper with styled

--- src/Pages/AuthWrapper.js
+++ src/Pages/AuthWrapper.js
@@ -1,9 +1,33 @@
 import React from "react";
-export const AuthWrapper = () => {
-    return (
-        <div>
-
-        </div>
-    );
+import { useAuth0 } from "@auth0/auth0-react";
+import { FaCentercode } from "react-icons/fa";
+import styled from 'styled-components'
+const AuthWrapper = ({ children }) => {
+    const { isLoading, error } = useAuth0()
+    if (isLoading) {
+        return (
+            <Wrapper>
+                <h1>Loading...</h1>
+            </Wrapper>
+        )
+    }
+    if (error) {
+        return (
+            <Wrapper>
+                <h1>{error.message}</h1>
+            </Wrapper>
+        )
+    }
+    return <>{children}</>
 }
+
+const Wrapper = styled.section`
+min-height: 100vh,
+display: grid;
+justify-content: center;
+margin:auto;
+`
+
+
+export default AuthWrapper
 
