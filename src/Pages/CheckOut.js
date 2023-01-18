fde2f034d5a96b9ab9d3ac932b6f3fbc9083b700
Daniel <danielu96@wp.pl>
2023-01-18 13:23:09 +0100

    if (amount < 1) added

--- src/Pages/CheckOut.js
+++ src/Pages/CheckOut.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import "../Styles/Css/Cart.css";
+
 const CheckOut = () => {
+    const { cartItems, amount, total } = useSelector((state) => state.cart);
+    if (amount < 1) {
+        return (
+            <>
+                <div style={{ textAlign: "center", margin: "2rem auto 4rem auto" }}>
+                    <h1 >Koszyk jest pusty</h1>
+                    <div style={{ margin: "2rem auto 4rem auto", alignContent: "center" }}>
+                        <Link className="btn" to="/Products" >Kup coś</Link>
+                    </div>
+                </div>
+            </>
+        );
+    }
     return (
         <div style={{ textAlign: "center", margin: "2rem auto 2rem auto" }}><h1>CheckOut here</h1></div>
     );
