cb8acf23fe32ee8bdc524fdcc1ee9e314c8ba82a
Daniel <danielu96@wp.pl>
2023-01-18 11:42:53 +0100

    style added

--- src/Pages/CheckOut.js
+++ src/Pages/CheckOut.js
@@ -1,6 +1,7 @@
+import React from "react";
 const CheckOut = () => {
     return (
-        <div>CheckOut</div>
+        <div style={{ textAlign: "center", margin: "2rem auto 2rem auto" }}><h1>CheckOut here</h1></div>
     );
 }
 
