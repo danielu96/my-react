08db0e08cdae11c6355f2e90ac91e9211992af7c
Daniel <danielu96@wp.pl>
2023-01-19 14:20:09 +0100

    user added

--- src/Pages/CheckOut.js
+++ src/Pages/CheckOut.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import "../Styles/Css/Cart.css";
 
+
 const CheckOut = () => {
     const { cartItems, amount, total } = useSelector((state) => state.cart);
+    const [myUser, setMyUser] = useState(null);
+    const { isAuthenticated, logout, loginWithRedirect, user } = useAuth0()
+    useEffect(() => {
+        setMyUser(user)
+    }, [user])
     if (amount < 1) {
         return (
             <>
@@ -18,7 +26,14 @@ const CheckOut = () => {
         );
     }
     return (
-        <div style={{ textAlign: "center", margin: "2rem auto 2rem auto" }}><h1>CheckOut here</h1></div>
+        <>
+            <div style={{ textAlign: "center", margin: "2rem auto 2rem auto" }}><h1>Hello</h1>
+                <h1> {myUser && myUser.name}</h1>
+                <span>do zapłaty {total} zł</span>
+                <button style={{ marginTop: "2rem" }} className="btn">Pay</button>
+            </div>
+
+        </>
     );
 }
 
