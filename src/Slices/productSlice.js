ae9c5d8c0066074b16ff711ca651b193f9e2a700
Daniel <danielu96@wp.pl>
2023-02-08 11:03:52 +0100

    tempCartItems changed to tempProducts

--- src/Slices/productSlice.js
+++ src/Slices/productSlice.js
@@ -23,29 +23,29 @@ const productsSlice = createSlice({
 
         updateSort: (state, action) => {
             const { sort, FilterProducts } = state;
-            let tempCartItems = [...FilterProducts]
+            let tempProducts = [...FilterProducts]
 
             if (sort === 'price-lowest') {
-                tempCartItems = tempCartItems.sort((a, b) => b.cena - a.cena)
+                tempProducts = tempProducts.sort((a, b) => b.cena - a.cena)
                 console.log("najnizsza wartosc")
             }
             if (sort === 'price-highest') {
-                tempCartItems = tempCartItems.sort((a, b) => a.cena - b.cena)
+                tempProducts = tempProducts.sort((a, b) => a.cena - b.cena)
                 console.log("najwyzsza wartaaosc")
             }
             if (sort === 'name-a') {
-                tempCartItems = tempCartItems.sort((a, b) => {
+                tempProducts = tempProducts.sort((a, b) => {
                     return a.title.localeCompare(b.title)
                 })
                 console.log('od a do z ')
             }
             if (sort === 'name-z') {
-                tempCartItems = tempCartItems.sort((a, b) => {
+                tempProducts = tempProducts.sort((a, b) => {
                     return b.title.localeCompare(a.title)
                 })
                 console.log('od z do a')
             }
-            return { ...state, sort: action.payload, FilterProducts: tempCartItems }
+            return { ...state, sort: action.payload, FilterProducts: tempProducts }
         }
     }
 }
