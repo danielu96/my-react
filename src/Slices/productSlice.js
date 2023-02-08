aa915d4646fe058eb8eb5907b7d9262e9f6cc08c
Daniel <danielu96@wp.pl>
2023-02-08 22:13:32 +0100

    updateFilters added

--- src/Slices/productSlice.js
+++ src/Slices/productSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../Cart/cartItems';
 
 
 const initialState = {
-    // cartItems: cartItems,
+    all_products: cartItems,
     FilterProducts: cartItems,
     sort: "price-lowest",
     filters: {
@@ -23,32 +23,37 @@ const productsSlice = createSlice({
 
         updateSort: (state, action) => {
             const { sort, FilterProducts } = state;
-            let tempCartItems = [...FilterProducts]
+            let tempProducts = [...FilterProducts]
 
             if (sort === 'price-lowest') {
-                tempCartItems = tempCartItems.sort((a, b) => b.cena - a.cena)
+                tempProducts = tempProducts.sort((a, b) => b.cena - a.cena)
                 console.log("najnizsza wartosc")
             }
             if (sort === 'price-highest') {
-                tempCartItems = tempCartItems.sort((a, b) => a.cena - b.cena)
+                tempProducts = tempProducts.sort((a, b) => a.cena - b.cena)
                 console.log("najwyzsza wartaaosc")
             }
             if (sort === 'name-a') {
-                tempCartItems = tempCartItems.sort((a, b) => {
+                tempProducts = tempProducts.sort((a, b) => {
                     return a.title.localeCompare(b.title)
                 })
                 console.log('od a do z ')
             }
             if (sort === 'name-z') {
-                tempCartItems = tempCartItems.sort((a, b) => {
+                tempProducts = tempProducts.sort((a, b) => {
                     return b.title.localeCompare(a.title)
                 })
                 console.log('od z do a')
             }
-            return { ...state, sort: action.payload, FilterProducts: tempCartItems }
-        }
+            return { ...state, sort: action.payload, FilterProducts: tempProducts }
+        },
+        updateFilters: (state, action) => {
+            const { name, value } = action.payload
+            return { ...state, filters: { ...state.filters, [name]: value } }
+        },
+        clearFilters: () => { },
     }
 }
 )
-export const { sort, updateSort, FilterProducts } = productsSlice.actions;
+export const { sort, updateSort, FilterProducts, updateFilters, clearFilters, text, name } = productsSlice.actions;
 export default productsSlice.reducer;
