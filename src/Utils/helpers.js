48ce8872aff14d1014e42f000101a43979fdbecb
Daniel <danielu96@wp.pl>
2023-02-13 11:32:16 +0100

    unique.flat disabled

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -1,7 +1,11 @@
+import { data } from '../Cart/cartItems'
+
 export const getUniqueValues = (data, type) => {
     let unique = data.map((item) => item[type])
-    if (type === 'colors') {
-        unique = unique.flat()
-    }
+    // console.log(unique)
+    // if (type === 'colors') {
+    //     unique = unique.flat()
+    //    
+    // }
     return ['all', ...new Set(unique)]
 }
