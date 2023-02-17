a619c398b392d302aa67b95ba4b87aafad61115e
Daniel <danielu96@wp.pl>
2023-02-17 11:41:59 +0100

    display fixed

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { sort, updateSort, Load_Products } from "../Slices/productSlice";
+import { sort, updateSort, Load_Products, SortProducts, updateFilters } from "../Slices/productSlice";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaMugHot, FaTimes } from "react-icons/fa";
@@ -7,9 +7,6 @@ import { useParams, useNavigate } from "react-router-dom";
 import "../Styles/Css/App.css";
 import ReactPaginate from "react-paginate";
 import Filters from "../Components/Filters";
-
-
-
 import {
     MDBCard,
     MDBCardTitle,
@@ -19,6 +16,7 @@ import {
 } from 'mdb-react-ui-kit';
 import cartItems from "../Cart/cartItems";
 const Products = ({ data }) => {
+    // const { state } = useSelector((state) => state.products);
     const { title } = useParams();
     const toggleShow = () => setCentredModal(!centredModal);
     const [centredModal, setCentredModal] = useState(false);
@@ -29,85 +27,132 @@ const Products = ({ data }) => {
     const itemsPerPage = 6;
     const dispatch = useDispatch()
     const { FilterProducts } = useSelector((state) => state.products);
+    // const { FilterProducts } = useSelector((state) => state.products);
+    // const [filters] = useSelector((state) => state.filters)
+    // const [sort] = useSelector((state) => state.sort)
+
+    const initialState = {
+        FilterProducts: [],
+        all_products: [],
+        // grid_view: true,
+        sort: 'price-lowest',
+        filters: {
+            text: '',
+            company: 'all',
+            category: 'all',
+            color: 'all',
+            min_cena: 0,
+            max_cena: 0,
+            cena: 0,
+            shipping: false,
+        },
+    }
     useEffect(() => {
         dispatch(Load_Products(data));
     }, [data]);
+    // useEffect(() => {
+    //     dispatch(SortProducts(initialState));
+    //     dispatch(updateFilters(initialState));
+    // }, [sort, filters]);
+
+    // useEffect(() => {
+    //     // dispatch({ updateFilters })
+    //     dispatch({ handleUpdateSort })
+    // }, [products,
+    //     state.sort,
+    //     state.filters
+    // ])
+
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
         setCurrentItems(FilterProducts.slice(itemOffset, endOffset));
         setPageCount(Math.ceil(FilterProducts.length / itemsPerPage));
-    }, [itemOffset, itemsPerPage, data, FilterProducts]);
+    }, [itemOffset, itemsPerPage, data,
+
+        FilterProducts
+    ]);
     const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % data.length;
+        const newOffset = (event.selected * itemsPerPage) %
+            FilterProducts.length;
         console.log(
             `User requested page number ${event.selected}, which is offset ${newOffset}`
         );
         setItemOffset(newOffset);
     };
     const handleUpdateSort = (e) => {
-        const name = e.target.name
+        // const name = e.target.name
         const value = e.target.value
-        console.log(name, value)
-        dispatch(updateSort(value, name))
+        // console.log(name, value)
+        dispatch(SortProducts(value,
+            //  name
+        ))
     }
     return (
         <>
-            <div style={{ textAlign: "center", paddingTop: "3rem", fontFamily: "impact", color: "gray" }}>
+
+            <div className="container"
+                style={{ textAlign: "center", paddingTop: "0", fontFamily: "impact", color: "gray" }}
+            >
                 <i className="fa fa-coffee fa-3x" aria-hidden="true"></i><h1>Kubki z nadrukiem</h1></div>
-            <Filters />
-            <div className="container" style={{ margin: "0 auto 0 auto" }}>
-                <div className="product-item">
-                    <div className="item-products">
-                        <p>{data.length} product found</p>
+            <div className="filter-products">
+                <Filters />
+                <div >
+                    <div
+                        className="container" style={{ margin: "0 auto 0 auto", height: "auto" }}
+                    >
+                        <div className="product-item">
+                            <div className="item-products">
+                                <p>{data.length} product found</p>
+                            </div>
+                            <hr className="item-hr" />
+                            <form className="item-price" style={{ minHeight: "auto" }}>
+                                <label className="label" htmlFor='sort'>sort by</label>
+                                <select name="sort" id="sort" className="sort-input"
+                                    value={sort}
+                                    onChange={handleUpdateSort}>
+                                    <option value="price-lowest">price lowest</option>
+                                    <option value="price-highest">price highest</option>
+                                    <option value="name-a">name(a-z)</option>
+                                    <option value="name-z">name(z-a)</option>
+                                </select>
+                            </form>
+                        </div>
                     </div>
-                    <hr className="item-hr" />
-                    <form className="item-price" style={{ minHeight: "auto" }}>
-                        <label className="label" htmlFor='sort'>sort by</label>
-                        <select name="sort" id="sort" className="sort-input"
-                            value={sort}
-                            onChange={handleUpdateSort}>
-                            <option value="price-lowest">price lowest</option>
-                            <option value="price-highest">price highest</option>
-                            <option value="name-a">name(a-z)</option>
-                            <option value="name-z">name(z-a)</option>
-                        </select>
-                    </form>
+                    <div className="box" style={{ margin: "0 ", paddingBottom: "0", minHeight: "80%", Width: "auto", paddingTop: '0.3rem' }}>
+                        {currentItems
+                            .map
+                            ((card, id) => (
+                                <MDBCard style={{ background: "white" }} className='text-gray mb-3' key={id}>< FaMugHot length="2x" style={{ color: "lightgray", width: "20px", margin: "auto" }} />
+                                    <MDBCardHeader > <h1 >{card.title}</h1>
+                                        <span>{card.cena}</span>
+                                    </MDBCardHeader>
+                                    <MDBCardBody>
+                                        <MDBCardTitle> {card.description}</MDBCardTitle>
+                                        <MDBCardText style={{ color: "gray" }}>
+                                            {card.opis}
+                                        </MDBCardText>
+                                    </MDBCardBody>
+                                    <Link className=" btn" style={{ marginLeft: "1rem" }} to={`/Produkty/${card.title}`}  > Zobacz</Link>
+                                </MDBCard>
+                            ))}
+                    </div>
+                    <ReactPaginate
+                        breakLabel="..."
+                        nextLabel="next >"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={5}
+                        pageCount={pageCount}
+                        previousLabel="< previous"
+                        renderOnZeroPageCount={null}
+                        containerClassName={"pagination"}
+                        pageLinkClassName={"page-num"}
+                        previousLinkClassName={"page-num"}
+                        nextLinkClassName={"page-num"}
+                        disabledClassName={"disabled"}
+                        activeClassName={"active"}
+                    />
                 </div>
             </div>
-            <div className="box">
-                {currentItems
-                    .map
-                    ((card, id) => (
-                        <MDBCard style={{ background: "white" }} className='text-gray mb-3' key={id}>< FaMugHot length="2x" style={{ color: "lightgray", width: "20px", margin: "auto" }} />
-                            <MDBCardHeader > <h1 >{card.title}</h1>
-                                <span>{card.cena}</span>
-                            </MDBCardHeader>
-                            <MDBCardBody>
-                                <MDBCardTitle> {card.description}</MDBCardTitle>
-                                <MDBCardText style={{ color: "gray" }}>
-                                    {card.opis}
-                                </MDBCardText>
-                            </MDBCardBody>
-                            <Link className=" btn" style={{ marginLeft: "1rem" }} to={`/Produkty/${card.title}`}  > Zobacz</Link>
-                        </MDBCard>
-                    ))}
-            </div>
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={5}
-                pageCount={pageCount}
-                previousLabel="< previous"
-                renderOnZeroPageCount={null}
-                containerClassName={"pagination"}
-                pageLinkClassName={"page-num"}
-                previousLinkClassName={"page-num"}
-                nextLinkClassName={"page-num"}
-                disabledClassName={"disabled"}
-                activeClassName={"active"}
-            />
-
         </>
     );
 };
