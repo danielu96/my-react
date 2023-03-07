c9969bbace48aede321e64732baa83c57c086b58
Daniel <danielu96@wp.pl>
2023-03-07 17:29:56 +0100

    fixed

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,11 +1,31 @@
-import React from 'react'
+// import React from 'react'
+import React, { useEffect, useState, useReducer } from "react";
+// import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { useFilterContext } from '../context/filter_context'
+// import { useFilterContext } from '../context/filter_context'
 import GridView from './GridView'
 import ListView from './ListView'
 const ProductList = () => {
-    // const { filtered_products: products, grid_view } = useFilterContext()
-    const { filtered_products: products, grid_view } = useSelector((state) => state.products);
+
+    const { filtered_products: products, grid_view } = useSelector((store) => store.products);
+    const dispatch = useDispatch();
+
+
+
+    // useEffect(()=>{
+    //     dispatch
+    // })
+    // const [data, setData] = useState([products]);
+    // const apiGet = () => {
+    //     fetch("../DATA2/data2.json")
+    //         .then((response) => response.json())
+    //         .then((json) => {
+    //             setData(json);
+    //         });
+    // };
+    // useEffect(() => {
+    //     apiGet(products);
+    // }, [products]);
 
     if (products.length < 1) {
         return (
