c357ac78a4fd3fac6547215568547d1fbeee1c96
Daniel <danielu96@wp.pl>
2023-03-07 17:24:31 +0100

    Test.js created

---
+++ src/Pages/Test.js
@@ -0,0 +1,85 @@
+// import { JobsContainer } from '../components/JobsContainer';
+
+// import SearchContainer from '../components/SearchContainer'
+import { ClassNames } from '@emotion/react';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../Slices/productSlice';
+
+
+const Test = () => {
+    // const [todos, setTodos] = useState([]);
+    // useEffect(() => {
+    //     axios.get("https://jsonplaceholder.typicode.com/todos")
+    //         .then(res => {
+    //             console.log(res)
+    //             setTodos(res.data)
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
+    // }, [])
+
+
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchUsers())
+        console.log(user)
+    }, [])
+    return (
+
+        <>
+            <div className='container'>
+                <h1>LISTA</h1>
+                {/* <ul>
+                    {todos.map((todo) => (
+
+                        <li key={todo.id}>{todo.title}</li>
+
+                    ))
+                    }
+                </ul> */}
+                {user.loading && <div>Loading... </div>}
+                {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+                {!user.loading && user.users.lenght ? (
+                    <ul>
+                        {user.users.map((item) => (
+                            <li key={item.id}>{item.name}</li>
+                        ))}
+                    </ul>
+                ) : null}
+            </div>
+
+
+            {/* <SearchContainer /> */}
+            {/* <JobsContainer /> */}
+        </>
+    )
+}
+
+export default Test
+
+ // const apiGet = () => {
+    //     fetch(".https://jsonplaceholder.typicode.com/todos")
+    //         .then((response) => response.json())
+    //         .then((json) => {
+    //             setData(json);
+    //         });
+    // };
+    // useEffect(() => {
+    //     apiGet();
+    // }, []);
+
+    // fetch('https://jsonplaceholder.typicode.com/todos')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+
+    // export const fetchProducts = createAsyncThunk('product/fetchProducts', () => {
+//     return axios
+//         .get("../Data2/data2.json")
+//         .then((response) => response.data.map((product) => product.id))
+//     // console.log(data)
+// })
