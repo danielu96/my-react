8c1456ea7733c9451c775e18b6f3bac12e69a30e
Daniel <danielu96@wp.pl>
2023-03-07 17:18:25 +0100

    cleaning

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import store from './store/store';
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
+
 
 
 
@@ -16,19 +17,16 @@ root.render(
     clientId="eHFy6F4nHjApEpt9ouH69onSZUn2VnIS"
     redirectUri={window.location.origin}
     cacheLocation="localstorage">
-    {/* <UserProvider> */}
+
     <React.StrictMode>
       <Provider store={store}>
-
         <App />
-
       </Provider>
     </React.StrictMode>
-    {/* </UserProvider> */}
   </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
