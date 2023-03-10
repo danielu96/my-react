8f5303982575165c389dd47567f07b817a5f50f9
Daniel <danielu96@wp.pl>
2023-03-10 17:01:18 +0100

    userSlice deleted

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { configureStore, createReducer } from '@reduxjs/toolkit'
 import cartReducer from '../Cart/cartSlice';
 import modalReducer from '../Cart/modalSlice';
 import productReducer from '../Slices/productSlice';
-import userSlice from '../Slices/userSlice'
+// import userSlice from '../Slices/userSlice'
 
 
 const store = configureStore({
@@ -10,7 +10,7 @@ const store = configureStore({
     cart: cartReducer,
     product: productReducer,
     modal: modalReducer,
-    user: userSlice,
+    // user: userSlice,
 
   },
 });
