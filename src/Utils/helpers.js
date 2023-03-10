a789998789cd89a86202037a44c4a8e90f3de278
Daniel <danielu96@wp.pl>
2023-03-10 16:54:06 +0100

    formatPrice added

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -1,4 +1,11 @@
-import { data } from '../Cart/cartItems'
+
+
+export const formatPrice = (number) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(number / 100)
+}
 
 export const getUniqueValues = (data, type) => {
     let unique = data.map((item) => item[type])
