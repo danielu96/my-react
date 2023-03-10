7f709cce103fea44a62ac760af7fc84c31d516c7
Daniel <danielu96@wp.pl>
2023-03-10 16:57:08 +0100

    change on productReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,14 @@
 import { configureStore, createReducer } from '@reduxjs/toolkit'
 import cartReducer from '../Cart/cartSlice';
 import modalReducer from '../Cart/modalSlice';
-import productsSlice from '../Slices/productSlice';
+import productReducer from '../Slices/productSlice';
 import userSlice from '../Slices/userSlice'
 
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    products: productsSlice,
+    product: productReducer,
     modal: modalReducer,
     user: userSlice,
 
