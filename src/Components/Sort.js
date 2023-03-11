abffe6595c73777d24b3141951e44faf21ab6b2c
Daniel <danielu96@wp.pl>
2023-03-11 18:38:49 +0100

    new Sort with useFilterContext() added

--- src/Components/Sort.js
+++ src/Components/Sort.js
@@ -1,189 +1,308 @@
-import react from 'react';
-import { set_GridView, set_ListView } from '../Slices/productSlice';
-import { useDispatch, useSelector } from "react-redux";
+// import react from 'react';
+import React from 'react'
+import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
-import { handleChange } from '../Slices/productSlice';
+import styled from 'styled-components'
 const Sort = () => {
-
   const {
-    products,
+    filtered_products: products,
     grid_view,
+    setGridView,
+    setListView,
     sort,
     updateSort,
-  } = useSelector((state) => state.product);
-
-
-  const dispatch = useDispatch()
-  const handleUpdateSort = (e) => {
-    // const name = e.target.name
-    const value = e.target.value
-    console.log(value)
-    dispatch(updateSort(value,
-      //  name
-    ))
-  }
-  const handleSearch = (e) => {
-    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
-  };
-  const handleSetGridView = () => {
-    console.log('grid')
-    dispatch(set_GridView())
-  }
-  const handleSetListView = () => {
-    console.log('list')
-    dispatch(set_ListView())
-  }
+  } = useFilterContext()
   return (
-    <div className="product-item">
-      <div >
+    <Wrapper>
+      <div className='btn-container'>
         <button
-          onClick={handleSetGridView}
+          onClick={setGridView}
           className={`${grid_view ? 'active' : null}`}
         >
           <BsFillGridFill />
         </button>
         <button
-          onClick={handleSetListView}
+          onClick={setListView}
           className={`${!grid_view ? 'active' : null}`}
         >
           <BsList />
         </button>
       </div>
-      <div className="item-products">
-        <p>{products.length} product found</p>
-      </div>
-      <hr className="item-hr" />
-      <form className="item-price" style={{ minHeight: "auto" }}>
-        <label className="label" htmlFor='sort'>sort by</label>
-        <select name="sort" id="sort" className="sort-input"
+      <p>{products.length} products found</p>
+      <hr />
+      <form>
+        <label htmlFor='sort'>sort by</label>
+        <select
+          name='sort'
+          id='sort'
           value={sort}
-          onChange={handleUpdateSort}
-        // onChange={handleChange}
+          onChange={updateSort}
+          className='sort-input'
         >
-          <option value="price-lowest">price lowest</option>
-          <option value="price-highest">price highest</option>
-          <option value="name-a">name(a-z)</option>
-          <option value="name-z">name(z-a)</option>
+          <option value='price-lowest'>price (lowest)</option>
+          <option value='price-highest'>price (highest)</option>
+          <option value='name-a'>name (a - z)</option>
+          <option value='name-z'>name (z - a)</option>
         </select>
       </form>
-    </div>
-  );
+    </Wrapper>
+  )
 }
 
-export default Sort;
+const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: auto auto 1fr auto;
+  align-items: center;
+  margin-bottom: 2rem;
+  column-gap: 2rem;
+  @media (max-width: 576px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 0.75rem;
+    .btn-container {
+      width: 50px;
+    }
+    label {
+      display: inline-block;
+      margin-right: 0.5rem;
+    }
+  }
+  @media (min-width: 768px) {
+    column-gap: 2rem;
+  }
+  p {
+    text-transform: capitalize;
+    margin-bottom: 0;
+  }
+
+  .btn-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 0.5rem;
+    button {
+      background: transparent;
+      border: 1px solid var(--clr-black);
+      color: var(--clr-black);
+      width: 1.5rem;
+      border-radius: var(--radius);
+      height: 1.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      svg {
+        font-size: 1rem;
+      }
+    }
+    .active {
+      background: var(--clr-black);
+      color: var(--clr-white);
+    }
+  }
+
+  .sort-input {
+    border-color: transparent;
+    font-size: 1rem;
+    text-transform: capitalize;
+    padding: 0.25rem 0.5rem;
+  }
+  label {
+    font-size: 1rem;
+    text-transform: capitalize;
+  }
+`
+
+export default Sort
 
 
 
 
-// import React from 'react'
-// import { useFilterContext } from '../context/filter_context'
+
+// import { set_GridView, set_ListView } from '../Slices/productSlice';
+// import { useDispatch, useSelector } from "react-redux";
 // import { BsFillGridFill, BsList } from 'react-icons/bs'
-// import styled from 'styled-components'
+// import { handleChange } from '../Slices/productSlice';
 // const Sort = () => {
+
 //   const {
-//     filtered_products: products,
+//     products,
 //     grid_view,
-//     setGridView,
-//     setListView,
 //     sort,
 //     updateSort,
-//   } = useFilterContext()
+//   } = useSelector((state) => state.product);
+
+
+//   const dispatch = useDispatch()
+//   const handleUpdateSort = (e) => {
+//     // const name = e.target.name
+//     const value = e.target.value
+//     console.log(value)
+//     dispatch(updateSort(value,
+//       //  name
+//     ))
+//   }
+//   const handleSearch = (e) => {
+//     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+//   };
+//   const handleSetGridView = () => {
+//     console.log('grid')
+//     dispatch(set_GridView())
+//   }
+//   const handleSetListView = () => {
+//     console.log('list')
+//     dispatch(set_ListView())
+//   }
 //   return (
-//     <Wrapper>
-//       <div className='btn-container'>
+//     <div className="product-item">
+//       <div >
 //         <button
-//           onClick={setGridView}
+//           onClick={handleSetGridView}
 //           className={`${grid_view ? 'active' : null}`}
 //         >
 //           <BsFillGridFill />
 //         </button>
 //         <button
-//           onClick={setListView}
+//           onClick={handleSetListView}
 //           className={`${!grid_view ? 'active' : null}`}
 //         >
 //           <BsList />
 //         </button>
 //       </div>
-//       <p>{products.length} products found</p>
-//       <hr />
-//       <form>
-//         <label htmlFor='sort'>sort by</label>
-//         <select
-//           name='sort'
-//           id='sort'
+//       <div className="item-products">
+//         <p>{products.length} product found</p>
+//       </div>
+//       <hr className="item-hr" />
+//       <form className="item-price" style={{ minHeight: "auto" }}>
+//         <label className="label" htmlFor='sort'>sort by</label>
+//         <select name="sort" id="sort" className="sort-input"
 //           value={sort}
-//           onChange={updateSort}
-//           className='sort-input'
+//           onChange={handleUpdateSort}
+//         // onChange={handleChange}
 //         >
-//           <option value='price-lowest'>price (lowest)</option>
-//           <option value='price-highest'>price (highest)</option>
-//           <option value='name-a'>name (a - z)</option>
-//           <option value='name-z'>name (z - a)</option>
+//           <option value="price-lowest">price lowest</option>
+//           <option value="price-highest">price highest</option>
+//           <option value="name-a">name(a-z)</option>
+//           <option value="name-z">name(z-a)</option>
 //         </select>
 //       </form>
-//     </Wrapper>
-//   )
+//     </div>
+//   );
 // }
 
-// const Wrapper = styled.section`
-//   display: grid;
-//   grid-template-columns: auto auto 1fr auto;
-//   align-items: center;
-//   margin-bottom: 2rem;
-//   column-gap: 2rem;
-//   @media (max-width: 576px) {
-//     display: grid;
-//     grid-template-columns: 1fr;
-//     row-gap: 0.75rem;
-//     .btn-container {
-//       width: 50px;
-//     }
-//     label {
-//       display: inline-block;
-//       margin-right: 0.5rem;
-//     }
-//   }
-//   @media (min-width: 768px) {
-//     column-gap: 2rem;
-//   }
-//   p {
-//     text-transform: capitalize;
-//     margin-bottom: 0;
-//   }
-//   .btn-container {
-//     display: grid;
-//     grid-template-columns: 1fr 1fr;
-//     column-gap: 0.5rem;
-//     button {
-//       background: transparent;
-//       border: 1px solid var(--clr-black);
-//       color: var(--clr-black);
-//       width: 1.5rem;
-//       border-radius: var(--radius);
-//       height: 1.5rem;
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//       cursor: pointer;
-//       svg {
-//         font-size: 1rem;
-//       }
-//     }
-//     .active {
-//       background: var(--clr-black);
-//       color: var(--clr-white);
-//     }
-//   }
-//   .sort-input {
-//     border-color: transparent;
-//     font-size: 1rem;
-//     text-transform: capitalize;
-//     padding: 0.25rem 0.5rem;
-//   }
-//   label {
-//     font-size: 1rem;
-//     text-transform: capitalize;
-//   }
-// `
+// export default Sort;
 
-// export default Sort
+
+
+
+// // import React from 'react'
+// // import { useFilterContext } from '../context/filter_context'
+// // import { BsFillGridFill, BsList } from 'react-icons/bs'
+// // import styled from 'styled-components'
+// // const Sort = () => {
+// //   const {
+// //     filtered_products: products,
+// //     grid_view,
+// //     setGridView,
+// //     setListView,
+// //     sort,
+// //     updateSort,
+// //   } = useFilterContext()
+// //   return (
+// //     <Wrapper>
+// //       <div className='btn-container'>
+// //         <button
+// //           onClick={setGridView}
+// //           className={`${grid_view ? 'active' : null}`}
+// //         >
+// //           <BsFillGridFill />
+// //         </button>
+// //         <button
+// //           onClick={setListView}
+// //           className={`${!grid_view ? 'active' : null}`}
+// //         >
+// //           <BsList />
+// //         </button>
+// //       </div>
+// //       <p>{products.length} products found</p>
+// //       <hr />
+// //       <form>
+// //         <label htmlFor='sort'>sort by</label>
+// //         <select
+// //           name='sort'
+// //           id='sort'
+// //           value={sort}
+// //           onChange={updateSort}
+// //           className='sort-input'
+// //         >
+// //           <option value='price-lowest'>price (lowest)</option>
+// //           <option value='price-highest'>price (highest)</option>
+// //           <option value='name-a'>name (a - z)</option>
+// //           <option value='name-z'>name (z - a)</option>
+// //         </select>
+// //       </form>
+// //     </Wrapper>
+// //   )
+// // }
+
+// // const Wrapper = styled.section`
+// //   display: grid;
+// //   grid-template-columns: auto auto 1fr auto;
+// //   align-items: center;
+// //   margin-bottom: 2rem;
+// //   column-gap: 2rem;
+// //   @media (max-width: 576px) {
+// //     display: grid;
+// //     grid-template-columns: 1fr;
+// //     row-gap: 0.75rem;
+// //     .btn-container {
+// //       width: 50px;
+// //     }
+// //     label {
+// //       display: inline-block;
+// //       margin-right: 0.5rem;
+// //     }
+// //   }
+// //   @media (min-width: 768px) {
+// //     column-gap: 2rem;
+// //   }
+// //   p {
+// //     text-transform: capitalize;
+// //     margin-bottom: 0;
+// //   }
+// //   .btn-container {
+// //     display: grid;
+// //     grid-template-columns: 1fr 1fr;
+// //     column-gap: 0.5rem;
+// //     button {
+// //       background: transparent;
+// //       border: 1px solid var(--clr-black);
+// //       color: var(--clr-black);
+// //       width: 1.5rem;
+// //       border-radius: var(--radius);
+// //       height: 1.5rem;
+// //       display: flex;
+// //       align-items: center;
+// //       justify-content: center;
+// //       cursor: pointer;
+// //       svg {
+// //         font-size: 1rem;
+// //       }
+// //     }
+// //     .active {
+// //       background: var(--clr-black);
+// //       color: var(--clr-white);
+// //     }
+// //   }
+// //   .sort-input {
+// //     border-color: transparent;
+// //     font-size: 1rem;
+// //     text-transform: capitalize;
+// //     padding: 0.25rem 0.5rem;
+// //   }
+// //   label {
+// //     font-size: 1rem;
+// //     text-transform: capitalize;
+// //   }
+// // `
+
+// // export default Sort
