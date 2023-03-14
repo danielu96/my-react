3dd20e973be4397316a4920997bbe16855ec827c
Daniel <danielu96@wp.pl>
2023-03-14 10:56:24 +0100

    className="product-item" added

--- src/Components/Sort.js
+++ src/Components/Sort.js
@@ -13,103 +13,112 @@ const Sort = () => {
     updateSort,
   } = useFilterContext()
   return (
-    <Wrapper>
-      <div className='btn-container'>
-        <button
-          onClick={setGridView}
-          className={`${grid_view ? 'active' : null}`}
-        >
-          <BsFillGridFill />
-        </button>
-        <button
-          onClick={setListView}
-          className={`${!grid_view ? 'active' : null}`}
-        >
-          <BsList />
-        </button>
+    <>
+      <div className="product-item">
+        <div className='btn-container' >
+          <button
+            onClick={setGridView}
+            className={`${grid_view ? 'active' : null}`}
+          >
+            <BsFillGridFill />
+          </button>
+          <button
+            onClick={setListView}
+            className={`${!grid_view ? 'active' : null}`}
+          >
+            <BsList />
+          </button>
+        </div>
+        <div className="item-products">
+          <p>{products.length} product found</p>
+        </div>
+        <hr className="item-hr" />
+        <form className="item-price" style={{ minHeight: "auto" }}>
+          <label className="label" htmlFor='sort'>sort by</label>
+          <select name="sort" id="sort" className="sort-input"
+            value={sort}
+            onChange={updateSort}
+          // onChange={handleChange}
+          >
+            <option value="price-lowest">price lowest</option>
+            <option value="price-highest">price highest</option>
+            <option value="name-a">name(a-z)</option>
+            <option value="name-z">name(z-a)</option>
+          </select>
+        </form>
       </div>
-      <p>{products.length} products found</p>
-      <hr />
-      <form>
-        <label htmlFor='sort'>sort by</label>
-        <select
-          name='sort'
-          id='sort'
-          value={sort}
-          onChange={updateSort}
-          className='sort-input'
-        >
-          <option value='price-lowest'>price (lowest)</option>
-          <option value='price-highest'>price (highest)</option>
-          <option value='name-a'>name (a - z)</option>
-          <option value='name-z'>name (z - a)</option>
-        </select>
-      </form>
-    </Wrapper>
+
+
+
+    </>
   )
 }
 
 const Wrapper = styled.section`
+display: grid;
+grid - template - columns: auto auto 1fr auto;
+align - items: center;
+margin - bottom: 2rem;
+column - gap: 2rem;
+@media(max - width: 576px) {
   display: grid;
-  grid-template-columns: auto auto 1fr auto;
-  align-items: center;
-  margin-bottom: 2rem;
-  column-gap: 2rem;
-  @media (max-width: 576px) {
-    display: grid;
-    grid-template-columns: 1fr;
-    row-gap: 0.75rem;
-    .btn-container {
-      width: 50px;
-    }
+  grid - template - columns: 1fr;
+  row - gap: 0.75rem;
+    .btn - container {
+    width: 50px;
+  }
     label {
-      display: inline-block;
-      margin-right: 0.5rem;
-    }
+    display: inline - block;
+    margin - right: 0.5rem;
   }
-  @media (min-width: 768px) {
-    column-gap: 2rem;
-  }
+}
+@media(min - width: 768px) {
+  column - gap: 2rem;
+}
   p {
-    text-transform: capitalize;
-    margin-bottom: 0;
-  }
+  text - transform: capitalize;
+  margin - bottom: 0;
+}
 
-  .btn-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    column-gap: 0.5rem;
+  .btn - container {
+
+    
+
+
+  display: grid;
+  grid - template - columns: 1fr 1fr;
+  column - gap: 0.5rem;
     button {
-      background: transparent;
-      border: 1px solid var(--clr-black);
-      color: var(--clr-black);
-      width: 1.5rem;
-      border-radius: var(--radius);
-      height: 1.5rem;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
+    background: transparent;
+    border: 1px solid var(--clr - black);
+    color: var(--clr - black);
+    width: 1.5rem;
+    border - radius: var(--radius);
+    height: 1.5rem;
+    display: flex;
+    align - items: center;
+    justify - content: center;
+    cursor: pointer;
       svg {
-        font-size: 1rem;
-      }
+      font - size: 1rem;
     }
+  }
     .active {
-      background: var(--clr-black);
-      color: var(--clr-white);
-    }
+    background: var(--clr - black);
+    color: var(--clr - white);
   }
+}
 
-  .sort-input {
-    border-color: transparent;
-    font-size: 1rem;
-    text-transform: capitalize;
-    padding: 0.25rem 0.5rem;
-  }
+  .sort - input {
+  border - color: transparent;
+  font - size: 1rem;
+  text - transform: capitalize;
+  padding: 0.25rem 0.5rem;
+}
   label {
-    font-size: 1rem;
-    text-transform: capitalize;
-  }
+  font - size: 1rem;
+  text - transform: capitalize;
+}
 `
 
 export default Sort
@@ -157,13 +166,13 @@ export default Sort
 //       <div >
 //         <button
 //           onClick={handleSetGridView}
-//           className={`${grid_view ? 'active' : null}`}
+//           className={`${ grid_view ? 'active' : null } `}
 //         >
 //           <BsFillGridFill />
 //         </button>
 //         <button
 //           onClick={handleSetListView}
-//           className={`${!grid_view ? 'active' : null}`}
+//           className={`${ !grid_view ? 'active' : null } `}
 //         >
 //           <BsList />
 //         </button>
@@ -191,118 +200,3 @@ export default Sort
 
 // export default Sort;
 
-
-
-
-// // import React from 'react'
-// // import { useFilterContext } from '../context/filter_context'
-// // import { BsFillGridFill, BsList } from 'react-icons/bs'
-// // import styled from 'styled-components'
-// // const Sort = () => {
-// //   const {
-// //     filtered_products: products,
-// //     grid_view,
-// //     setGridView,
-// //     setListView,
-// //     sort,
-// //     updateSort,
-// //   } = useFilterContext()
-// //   return (
-// //     <Wrapper>
-// //       <div className='btn-container'>
-// //         <button
-// //           onClick={setGridView}
-// //           className={`${grid_view ? 'active' : null}`}
-// //         >
-// //           <BsFillGridFill />
-// //         </button>
-// //         <button
-// //           onClick={setListView}
-// //           className={`${!grid_view ? 'active' : null}`}
-// //         >
-// //           <BsList />
-// //         </button>
-// //       </div>
-// //       <p>{products.length} products found</p>
-// //       <hr />
-// //       <form>
-// //         <label htmlFor='sort'>sort by</label>
-// //         <select
-// //           name='sort'
-// //           id='sort'
-// //           value={sort}
-// //           onChange={updateSort}
-// //           className='sort-input'
-// //         >
-// //           <option value='price-lowest'>price (lowest)</option>
-// //           <option value='price-highest'>price (highest)</option>
-// //           <option value='name-a'>name (a - z)</option>
-// //           <option value='name-z'>name (z - a)</option>
-// //         </select>
-// //       </form>
-// //     </Wrapper>
-// //   )
-// // }
-
-// // const Wrapper = styled.section`
-// //   display: grid;
-// //   grid-template-columns: auto auto 1fr auto;
-// //   align-items: center;
-// //   margin-bottom: 2rem;
-// //   column-gap: 2rem;
-// //   @media (max-width: 576px) {
-// //     display: grid;
-// //     grid-template-columns: 1fr;
-// //     row-gap: 0.75rem;
-// //     .btn-container {
-// //       width: 50px;
-// //     }
-// //     label {
-// //       display: inline-block;
-// //       margin-right: 0.5rem;
-// //     }
-// //   }
-// //   @media (min-width: 768px) {
-// //     column-gap: 2rem;
-// //   }
-// //   p {
-// //     text-transform: capitalize;
-// //     margin-bottom: 0;
-// //   }
-// //   .btn-container {
-// //     display: grid;
-// //     grid-template-columns: 1fr 1fr;
-// //     column-gap: 0.5rem;
-// //     button {
-// //       background: transparent;
-// //       border: 1px solid var(--clr-black);
-// //       color: var(--clr-black);
-// //       width: 1.5rem;
-// //       border-radius: var(--radius);
-// //       height: 1.5rem;
-// //       display: flex;
-// //       align-items: center;
-// //       justify-content: center;
-// //       cursor: pointer;
-// //       svg {
-// //         font-size: 1rem;
-// //       }
-// //     }
-// //     .active {
-// //       background: var(--clr-black);
-// //       color: var(--clr-white);
-// //     }
-// //   }
-// //   .sort-input {
-// //     border-color: transparent;
-// //     font-size: 1rem;
-// //     text-transform: capitalize;
-// //     padding: 0.25rem 0.5rem;
-// //   }
-// //   label {
-// //     font-size: 1rem;
-// //     text-transform: capitalize;
-// //   }
-// // `
-
-// // export default Sort
