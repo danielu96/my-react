8c7a463db4a6b054e1f0a50968f296a2b48b1484
Daniel <danielu96@wp.pl>
2023-03-14 10:57:55 +0100

    image on Image changed

--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -5,22 +5,28 @@ import { Link } from 'react-router-dom'
 const ListView = ({ products }) => {
   return (
     <Wrapper>
-      {products.map((product) => {
-        const { id, image, name, price, description } = product
-        return (
-          <article key={id}>
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <h5 className='price'>{formatPrice(price)}</h5>
-              <p>{description.substring(0, 150)}...</p>
-              <Link to={`/products/${id}`} className='btn'>
-                Details
-              </Link>
-            </div>
-          </article>
-        )
-      })}
+      <div >
+        {
+          products.map((product) => {
+            const { id, Image, name, price, description, opis } = product
+            return (
+              <article style={{ marginTop: "1rem", height: 'auto', paddingTop: '1rem' }}
+                key={id}>
+                <img src={Image} alt={name} style={{ height: 'auto' }} />
+                <div>
+                  <h4>{name}</h4>
+                  <h5 className='price'>{price}</h5>
+                  {/* <p>{description.substring(0, 150)}...</p> */}
+                  <p>{opis}</p>
+                  <Link to={`/products/${id}`} className='btn'>
+                    Details
+                  </Link>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
     </Wrapper>
   )
 }
