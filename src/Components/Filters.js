9e241b4830034da796d3d2996b419ecad1f2a798
Daniel <danielu96@wp.pl>
2023-03-14 11:04:55 +0100

    className="form-controlen" added

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { useFilterContext } from '../context/filter_context'
 import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
+import { useDispatch, useSelector } from "react-redux";
+
 
 const Filters = () => {
   const {
@@ -24,66 +26,54 @@ const Filters = () => {
   const categories = getUniqueValues(all_products, 'category')
   const companies = getUniqueValues(all_products, 'company')
   const colors = getUniqueValues(all_products, 'colors')
+  const dispatch = useDispatch()
   return (
     <Wrapper>
-      <div className='content'>
-        <form onSubmit={(e) => e.preventDefault()}>
-          {/* search input */}
-          <div className='form-control'>
-            <input
-              type='text'
-              name='text'
-              value={text}
-              placeholder='search'
-              onChange={updateFilters}
-              className='search-input'
-            />
-          </div>
-          {/* end of search input */}
-          {/* category */}
-          <div className='form-control'>
-            <h5>category</h5>
+      <div
+        className="content"
+      // className="container" style={{ margin: "0 auto 0 auto" }}
+      >
+        <h4>Filters</h4>
+        <form className="form-controlen" style={{ minHeight: "1rem", width: "8rem" }}
+          onSubmit={(e) => e.preventDefault()}>
+          <input
+            type='text'
+            name="text"
+            placeholder="search"
+            value={text}
+            className="search-input"
+            onChange={updateFilters} />
+
+          <div className="form-controlen">
+            <h4>Category</h4>
             <div>
               {categories.map((c, index) => {
-                return (
-                  <button
-                    key={index}
-                    onClick={updateFilters}
-                    type='button'
-                    name='category'
-                    className={`${category === c.toLowerCase() ? 'active' : null
-                      }`}
-                  >
-                    {c}
-                  </button>
-                )
+                return <button key={index}
+                  onClick={updateFilters}
+                  type='button'
+                  name="category"
+                  className={`${category === c ? 'active' : null}`}
+                >{c}</button>
               })}
             </div>
           </div>
-          {/* end of category */}
-          {/* company */}
-          <div className='form-control'>
+          <div className="form-controlen">
             <h5>company</h5>
             <select
-              name='company'
+              name="company"
               value={company}
-              onChange={updateFilters}
               className='company'
-            >
+              onChange={updateFilters}>
               {companies.map((c, index) => {
-                return (
-                  <option key={index} value={c}>
-                    {c}
-                  </option>
-                )
+                return (<option key={index} value={c}>
+                  {c}
+                </option>)
               })}
             </select>
           </div>
-          {/* end of company */}
-          {/* colors */}
-          <div className='form-control'>
+          <div className="form-control">
             <h5>colors</h5>
-            <div className='colors'>
+            <div className="colors">
               {colors.map((c, index) => {
                 if (c === 'all') {
                   return (
@@ -92,46 +82,35 @@ const Filters = () => {
                       name='color'
                       onClick={updateFilters}
                       data-color='all'
-                      className={`${color === 'all' ? 'all-btn active' : 'all-btn'
-                        }`}
-                    >
-                      all
-                    </button>
+                      className={`${color === 'all' ? 'all-btn active' :
+                        'all-btn'}`}> all </button>
                   )
-                }
-                return (
-                  <button
+                } else {
+                  return <button
                     key={index}
                     name='color'
                     style={{ background: c }}
-                    className={`${color === c ? 'color-btn active' : 'color-btn'
-                      }`}
                     data-color={c}
-                    onClick={updateFilters}
-                  >
+                    className={`${color === c ? 'color-btn active' :
+                      'color-btn'}`}
+                    onClick={updateFilters}>
                     {color === c ? <FaCheck /> : null}
+
                   </button>
-                )
+                }
               })}
+
             </div>
           </div>
-          {/* end of colors */}
-          {/* price */}
-          <div className='form-control'>
-            <h5>price</h5>
-            <p className='price'>{formatPrice(price)}</p>
-            <input
-              type='range'
-              name='price'
+          <div className="price">
+            <h5>Price</h5>
+            <p>{price}</p>
+            <input type='range' name="price"
+              min={min_price} max={max_price}
               onChange={updateFilters}
-              min={min_price}
-              max={max_price}
-              value={price}
-            />
+              value={price} />
           </div>
-          {/* end of price */}
-          {/* shipping */}
-          <div className='form-control shipping'>
+          <div className='form-control shipping' style={{ border: 'none' }}>
             <label htmlFor='shipping'>free shipping</label>
             <input
               type='checkbox'
@@ -141,14 +120,24 @@ const Filters = () => {
               onChange={updateFilters}
             />
           </div>
-          {/* end of  shipping */}
+          <button type='button' className='clear-btn' onClick={clearFilters}>
+            clear filters
+          </button>
         </form>
-        <button type='button' className='clear-btn' onClick={clearFilters}>
+
+        {/* <button type='button' className='clear-btn'
+          // onClick={handleClearFilters}
+          onClick={() => {
+            dispatch(clearFilters())
+          }
+          }
+        >
           clear filters
-        </button>
+        </button> */}
+
       </div>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.section`
@@ -237,10 +226,12 @@ const Wrapper = styled.section`
     font-size: 1rem;
   }
   .clear-btn {
-    background: var(--clr-red-dark);
-    color: var(--clr-white);
+    width: fit-content;
+    background: red;
+    color: white;
     padding: 0.25rem 0.5rem;
-    border-radius: var(--radius);
+    border-radius: 5px;
+    float:none;
   }
   @media (min-width: 768px) {
     .content {
@@ -415,256 +406,4 @@ export default Filters
 // }
 
 // export default Filters;
-// // import React from 'react'
-// // import styled from 'styled-components'
-// // import { useFilterContext } from '../context/filter_context'
-// // import { getUniqueValues, formatPrice } from '../utils/helpers'
-// // import { FaCheck } from 'react-icons/fa'
 
-// // const Filters = () => {
-// //   const {
-// //     filters: {
-// //       text,
-// //       category,
-// //       company,
-// //       color,
-// //       min_cena,
-// //       max_cena,
-// //       cena,
-
-// //       shipping,
-// //     },
-// //     updateFilters,
-// //     all_products,
-// //     clearFilters,
-// //   } = useFilterContext()
-
-// //   const categories = getUniqueValues(all_products, 'category')
-// //   const companies = getUniqueValues(all_products, 'company')
-// //   const colors = getUniqueValues(all_products, 'colors')
-// //   return (
-// //     <Wrapper>
-// //       <div className='content'>
-// //         <form onSubmit={(e) => e.preventDefault()}>
-// //           {/* search input */}
-// //           <div className='form-control'>
-// //             <input
-// //               type='text'
-// //               name='text'
-// //               value={text}
-// //               placeholder='search'
-// //               onChange={updateFilters}
-// //               className='search-input'
-// //             />
-// //           </div>
-// //           {/* end of search input */}
-// //           {/* category */}
-// //           <div className='form-control'>
-// //             <h5>category</h5>
-// //             <div>
-// //               {categories.map((c, index) => {
-// //                 return (
-// //                   <button
-// //                     key={index}
-// //                     onClick={updateFilters}
-// //                     type='button'
-// //                     name='category'
-// //                     className={`${category === c.toLowerCase() ? 'active' : null
-// //                       }`}
-// //                   >
-// //                     {c}
-// //                   </button>
-// //                 )
-// //               })}
-// //             </div>
-// //           </div>
-// //           {/* end of category */}
-// //           {/* company */}
-// //           <div className='form-control'>
-// //             <h5>company</h5>
-// //             <select
-// //               name='company'
-// //               value={company}
-// //               onChange={updateFilters}
-// //               className='company'
-// //             >
-// //               {companies.map((c, index) => {
-// //                 return (
-// //                   <option key={index} value={c}>
-// //                     {c}
-// //                   </option>
-// //                 )
-// //               })}
-// //             </select>
-// //           </div>
-// //           {/* end of company */}
-// //           {/* colors */}
-// //           <div className='form-control'>
-// //             <h5>colors</h5>
-// //             <div className='colors'>
-// //               {colors.map((c, index) => {
-// //                 if (c === 'all') {
-// //                   return (
-// //                     <button
-// //                       key={index}
-// //                       name='color'
-// //                       onClick={updateFilters}
-// //                       data-color='all'
-// //                       className={`${color === 'all' ? 'all-btn active' : 'all-btn'
-// //                         }`}
-// //                     >
-// //                       all
-// //                     </button>
-// //                   )
-// //                 }
-// //                 return (
-// //                   <button
-// //                     key={index}
-// //                     name='color'
-// //                     style={{ background: c }}
-// //                     className={`${color === c ? 'color-btn active' : 'color-btn'
-// //                       }`}
-// //                     data-color={c}
-// //                     onClick={updateFilters}
-// //                   >
-// //                     {color === c ? <FaCheck /> : null}
-// //                   </button>
-// //                 )
-// //               })}
-// //             </div>
-// //           </div>
-// //           {/* end of colors */}
-// //           {/* price */}
-// //           <div className='form-control'>
-// //             <h5>cena</h5>
-// //             <p className='price'>{formatPrice(cena)}</p>
-// //             <input
-// //               type='range'
-// //               name='price'
-// //               onChange={updateFilters}
-// //               min={min_cena}
-// //               max={max_cena}
-// //               value={cena}
-// //             />
-// //           </div>
-// //           {/* end of price */}
-// //           {/* shipping */}
-// //           <div className='form-control shipping'>
-// //             <label htmlFor='shipping'>free shipping</label>
-// //             <input
-// //               type='checkbox'
-// //               name='shipping'
-// //               id='shipping'
-// //               checked={shipping}
-// //               onChange={updateFilters}
-// //             />
-// //           </div>
-// //           {/* end of  shipping */}
-// //         </form>
-// //         <button type='button' className='clear-btn' onClick={clearFilters}>
-// //           clear filters
-// //         </button>
-// //       </div>
-// //     </Wrapper>
-// //   )
-// // }
-
-// // const Wrapper = styled.section`
-// //   .form-control {
-// //     margin-bottom: 1.25rem;
-// //     h5 {
-// //       margin-bottom: 0.5rem;
-// //     }
-// //   }
-// //   .search-input {
-// //     padding: 0.5rem;
-// //     background: var(--clr-grey-10);
-// //     border-radius: var(--radius);
-// //     border-color: transparent;
-// //     letter-spacing: var(--spacing);
-// //   }
-// //   .search-input::placeholder {
-// //     text-transform: capitalize;
-// //   }
-// //   button {
-// //     display: block;
-// //     margin: 0.25em 0;
-// //     padding: 0.25rem 0;
-// //     text-transform: capitalize;
-// //     background: transparent;
-// //     border: none;
-// //     border-bottom: 1px solid transparent;
-// //     letter-spacing: var(--spacing);
-// //     color: var(--clr-grey-5);
-// //     cursor: pointer;
-// //   }
-// //   .active {
-// //     border-color: var(--clr-grey-5);
-// //   }
-// //   .company {
-// //     background: var(--clr-grey-10);
-// //     border-radius: var(--radius);
-// //     border-color: transparent;
-// //     padding: 0.25rem;
-// //   }
-// //   .colors {
-// //     display: flex;
-// //     align-items: center;
-// //   }
-// //   .color-btn {
-// //     display: inline-block;
-// //     width: 1rem;
-// //     height: 1rem;
-// //     border-radius: 50%;
-// //     background: #222;
-// //     margin-right: 0.5rem;
-// //     border: none;
-// //     cursor: pointer;
-// //     opacity: 0.5;
-// //     display: flex;
-// //     align-items: center;
-// //     justify-content: center;
-// //     svg {
-// //       font-size: 0.5rem;
-// //       color: var(--clr-white);
-// //     }
-// //   }
-// //   .all-btn {
-// //     display: flex;
-// //     align-items: center;
-// //     justify-content: center;
-// //     margin-right: 0.5rem;
-// //     opacity: 0.5;
-// //   }
-// //   .active {
-// //     opacity: 1;
-// //   }
-// //   .all-btn .active {
-// //     text-decoration: underline;
-// //   }
-// //   .price {
-// //     margin-bottom: 0.25rem;
-// //   }
-// //   .shipping {
-// //     display: grid;
-// //     grid-template-columns: auto 1fr;
-// //     align-items: center;
-// //     text-transform: capitalize;
-// //     column-gap: 0.5rem;
-// //     font-size: 1rem;
-// //   }
-// //   .clear-btn {
-// //     background: var(--clr-red-dark);
-// //     color: var(--clr-white);
-// //     padding: 0.25rem 0.5rem;
-// //     border-radius: var(--radius);
-// //   }
-// //   @media (min-width: 768px) {
-// //     .content {
-// //       position: sticky;
-// //       top: 1rem;
-// //     }
-// //   }
-// // `
-
-// // export default Filters
