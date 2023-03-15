d4fa291263e0701988fda4e0cf716909b470a931
Daniel <danielu96@wp.pl>
2023-03-15 14:24:33 +0100

    polskie nazwy kategorii added

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -29,12 +29,12 @@ const Filters = () => {
   const dispatch = useDispatch()
   return (
     <Wrapper>
-      <div className="filteren" >Filters</div>
+
       <div
         className="content"
       // className="container" style={{ margin: "0 auto 0 auto" }}
       >
-
+        <h3 className='filteren'>Filters</h3>
         <form className="form-controlen" style={{ minHeight: "1rem", width: "8rem" }}
 
           onSubmit={(e) => e.preventDefault()}>
@@ -46,8 +46,8 @@ const Filters = () => {
             className="search-input"
             onChange={updateFilters} />
 
-          <div className="form-controlen">
-            <h4>Category</h4>
+          <div className="form-controlen" style={{ marginBottom: '1rem' }}>
+            <h4>Kategorie</h4>
             <div>
               {categories.map((c, index) => {
                 return <button key={index}
@@ -60,7 +60,7 @@ const Filters = () => {
             </div>
           </div>
           <div className="form-controlen">
-            <h5>company</h5>
+            <h5>Produkt</h5>
             <select
               name="company"
               value={company}
@@ -74,7 +74,7 @@ const Filters = () => {
             </select>
           </div>
           <div className="form-controlen">
-            <h5>colors</h5>
+            <h5>Kolory</h5>
             <div className="colors">
               {colors.map((c, index) => {
                 if (c === 'all') {
@@ -97,23 +97,21 @@ const Filters = () => {
                       'color-btn'}`}
                     onClick={updateFilters}>
                     {color === c ? <FaCheck /> : null}
-
                   </button>
                 }
               })}
-
             </div>
           </div>
           <div className="price">
-            <h5>Price</h5>
-            <p>{price}</p>
+            <h5>Cena</h5>
+            <p>{price} zł</p>
             <input type='range' name="price"
               min={min_price} max={max_price}
               onChange={updateFilters}
               value={price} />
           </div>
           <div className='form-controlen shipping' style={{ border: 'none' }}>
-            <label htmlFor='shipping'>free shipping</label>
+            <label htmlFor='shipping'>dostawa</label>
             <input
               type='checkbox'
               name='shipping'
@@ -256,168 +254,5 @@ const Wrapper = styled.section`
 `
 
 export default Filters
-// import React, { useEffect, useState } from "react";
-// import { FaCheck } from "react-icons/fa";
-// import { useDispatch, useSelector } from "react-redux";
-// import { updateFilters, clearFilters, filterProducts } from "../Slices/productSlice";
-// import { getUniqueValues } from '../utils/helpers';
-// import "../Styles/Css/Filters.css";
-// // import cartItems from "../Cart/cartItems";
 
-// const Filters = () => {
-
-//   const {
-//     filters: {
-//       text,
-//       company,
-//       category,
-//       color,
-//       min_cena,
-//       max_cena,
-//       cena,
-//       shipping,
-//     },
-//     updateFilters,
-//     clearFilters,
-//     products,
-//     all_products,
-//     filtered_products
-//   } = useSelector((store) => store.product);
-//   const dispatch = useDispatch()
-
-
-//   const handleUpdateFilters = (e) => {
-//     let name = e.target.name
-//     let value = e.target.value
-//     if (name === 'category') {
-//       value = e.target.textContent
-//     }
-//     if (name === 'color') {
-//       value = e.target.dataset.color
-//     }
-//     if (name === 'shipping') {
-//       value = e.target.checked
-//     }
-//     // if (name === 'cena') {
-//     //     value = Number(value)
-//     // }
-
-//     console.log(name, value)
-//     dispatch(updateFilters({ name, value }))
-//   }
-
-
-//   const categories = getUniqueValues(products, 'category')
-//   const companies = getUniqueValues(products, 'company')
-//   const colors = getUniqueValues(products, 'color')
-//   // console.log(categories)
-
-
-
-//   return (<div
-//     className="content"
-//   // className="container" style={{ margin: "0 auto 0 auto" }}
-//   >
-//     <h4>Filters</h4>
-//     <form className="form-controlen" style={{ minHeight: "1rem", width: "8rem" }}
-//       onSubmit={(e) => e.preventDefault()}>
-//       <input
-//         type='text'
-//         name="text"
-//         placeholder="search"
-//         value={text}
-//         className="search-input"
-//         onChange={handleUpdateFilters} />
-
-//       <div className="form-controlen">
-//         <h4>Category</h4>
-//         <div>
-//           {categories.map((c, index) => {
-//             return <button key={index}
-//               onClick={handleUpdateFilters}
-//               type='button'
-//               name="category"
-//               className={`${category === c ? 'active' : null}`}
-//             >{c}</button>
-//           })}
-//         </div>
-//       </div>
-//       <div className="form-controlen">
-//         <h5>company</h5>
-//         <select
-//           name="company"
-//           value={company}
-//           className='company'
-//           onChange={handleUpdateFilters}>
-//           {companies.map((c, index) => {
-//             return (<option key={index} value={c}>
-//               {c}
-//             </option>)
-//           })}
-//         </select>
-//       </div>
-//       <div className="form-controlen">
-//         <h5>colors</h5>
-//         <div className="colors">
-//           {colors.map((c, index) => {
-//             if (c === 'all') {
-//               return (
-//                 <button
-//                   key={index}
-//                   name='color'
-//                   onClick={handleUpdateFilters}
-//                   data-color='all'
-//                   className={`${color === 'all' ? 'all-btn active' :
-//                     'all-btn'}`}> all </button>
-//               )
-//             } else {
-//               return <button
-//                 key={index}
-//                 name='color'
-//                 style={{ background: c }}
-//                 data-color={c}
-//                 className={`${color === c ? 'color-btn active' :
-//                   'color-btn'}`}
-//                 onClick={handleUpdateFilters}>
-//                 {color === c ? <FaCheck /> : null}
-
-//               </button>
-//             }
-//           })}
-
-//         </div>
-//       </div>
-//       <div className="price">
-//         <h5>Price</h5>
-//         <p>{cena}</p>
-//         <input type='range' name="cena"
-//           min={min_cena} max={max_cena}
-//           onChange={handleUpdateFilters}
-//           value={cena} />
-//       </div>
-//       <div className='form-control shipping' style={{ border: 'none' }}>
-//         <label htmlFor='shipping'>free shipping</label>
-//         <input
-//           type='checkbox'
-//           name='shipping'
-//           id='shipping'
-//           checked={shipping}
-//           onChange={handleUpdateFilters}
-//         />
-//       </div>
-//     </form>
-//     <button type='button' className='clear-btn'
-//       // onClick={handleClearFilters}
-//       onClick={() => {
-//         dispatch(clearFilters())
-//       }
-//       }
-//     >
-//       clear filters
-//     </button>
-
-//   </div>);
-// }
-
-// export default Filters;
 
